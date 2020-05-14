000e9aded08fa1d9819e05631af75a9c3003c64f
Mikisho <mikiye.82@gmail.com>
2020-05-14 16:03:02 -0500

    modified-testfile

--- src/__tests__/options.spec.js
+++ src/__tests__/options.spec.js
@@ -5,29 +5,28 @@ import { mount } from 'enzyme';
 import enzymeConfig from '../../enzymeConfig'
 
 const options = ['English', 'Spanish', 'German', 'French'];
-describe('<Options />', function() {
+describe('<Options />', function () {
 
-            it('should render correctly', function() {
-                        const component = renderer.create( < Options options = { options }
-                            />);
-                            const tree = component.toJSON(); expect(tree).toMatchSnapshot();
-                        });
+    it('should render correctly', function () {
+        const component = renderer.create(<Options options={options} />);
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
 
-                    it('should have the correct number of option tags', function() {
-                            const component = mount( < Options options = { options }
-                                />);
-                                expect(component.find('option').length).toEqual(4);
-                            });
+    it('should have the correct number of option tags', function () {
+        const component = mount(<Options options={options} />);
+        expect(component.find('option').length).toEqual(4);
+    });
 
-                        it('should render the correct option names', function() {
-                                let languages = '';
-                                const expected = 'EnglishSpanishGermanFrench';
-                                const component = mount( < Options options = { options }
-                                    />);
-                                    let optionArray = component.find('option');
+    it('should render the correct option names', function () {
+        let languages = '';
+        const expected = 'EnglishSpanishGermanFrench';
+        const component = mount(<Options options={options} />);
+        let optionArray = component.find('option');
 
-                                    optionArray.forEach((item) => {
-                                        languages += item.text();
-                                    }) expect(languages).toEqual(expected);
-                                });
-                        })
+        optionArray.forEach((item) => {
+            languages += item.text();
+        })
+        expect(languages).toEqual(expected);
+    });
+})
